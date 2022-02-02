0a95654c095a839760000525426647ad4ac713f9
John Irungu <johnydanxin@gmail.com>
2022-02-02 13:42:42 +0300

    Temporary Redeployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class]);
+        $this->call([
+            UsersTableSeeder::class,
+            ProductTableSeeder::class,
+            ServicesSeeder::class, 
+            VersionSeeder::class,
+            
+        ]);
     }
 }
